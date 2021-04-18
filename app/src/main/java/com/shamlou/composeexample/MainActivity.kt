dd86ce247f506d8a6d48d192953f2ced7b1868bc
keivanshamlu <keivan.shamlu.co@gmail.com>
2021-04-18 21:51:21 +0430

    init project

---
+++ app/src/main/java/com/shamlou/composeexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shamlou.composeexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
